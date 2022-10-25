e78eb7a40a239aff212667a996377f9012329224
Bestian Tang 小巴 <bestian@gmail.com>
2022-10-26 07:24:56 +0800

    s

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,28 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
+// Basic Flutter widget test. Learn more at https://flutter.io/docs/testing.
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:my_app/main.dart';
+import '/Users/bestian/Documents/GitHub/my_app/lib/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
+  testWidgets('Example app smoke test', (tester) async {
     await tester.pumpWidget(const MyApp());
+    expect(find.text('Oeschinen Lake Campground'), findsOneWidget);
+    expect(find.text('ROUTE'), findsOneWidget);
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    expect(find.byIcon(Icons.star), findsOneWidget);
+    expect(find.text('41'), findsOneWidget);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+    await tester.tap(find.byIcon(Icons.star));
     await tester.pump();
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.byIcon(Icons.star_border), findsOneWidget);
+    expect(find.text('40'), findsOneWidget);
+
+    await tester.tap(find.byIcon(Icons.star_border));
+    await tester.pump();
+
+    expect(find.byIcon(Icons.star), findsOneWidget);
+    expect(find.text('41'), findsOneWidget);
   });
 }
